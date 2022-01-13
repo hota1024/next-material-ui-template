2b91bb8fd44bd625ca2156cf56d2dbc17be83319
= <hotalog@hota1024.com>
2022-01-13 22:54:26 +0900

    📄add vanilla files

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import { NextPage } from 'next'
+import Head from 'next/head'
+
+/**
+ * HomePage component.
+ */
+export const HomePage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>home - next-material-ui-vannila-next</title>
+      </Head>
+
+      <div>next-material-ui-vannila-next</div>
+    </>
+  )
+}
+
+export default HomePage
